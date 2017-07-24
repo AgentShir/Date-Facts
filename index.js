09bc16cbb9768009ddce373403146e98a93aebe7
Shireen Garcia <garciasal@gmail.com>
2017-07-24 16:06:41 -0700

    Where things almost show up properly

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-moment().format(chalk.blue('MMMM Do YYYY, h:mm:ss a'));
-moment().format(chalk.magenta('dddd'));
+const chalk = require('chalk');
+
+console.log(
+    'It is ' + `${chalk.blue('MMMM Do YYYY, h:mm:ss a')}
+  `);
+
+
+// moment().format(chalk.blue('MMMM Do YYYY, h:mm:ss a'));
+// moment().format(chalk.magenta('dddd'));
