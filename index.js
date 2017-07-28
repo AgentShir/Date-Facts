aacd946cd9a8e94e24d0961b16b7e8f7e9a947e6
Shireen Garcia <garciasal@gmail.com>
2017-07-28 13:43:18 -0700

    Looking at Moment

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-const chalk = require('chalk');
-
-console.log(
-    'It is ' + `${chalk.blue('MMMM Do YYYY, h:mm:ss a')}.
-  `);
-
-
-// moment().format(chalk.blue('MMMM Do YYYY, h:mm:ss a'));
-// moment().format(chalk.magenta('dddd'));
+var moment = require('moment');
+moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
