217232d2153451741293c351a03c7204f4305ceb
Shireen Garcia <garciasal@gmail.com>
2017-07-30 14:49:19 -0700

    Finished

--- index.js
+++ index.js
@@ -3,7 +3,31 @@ moment().format();
 
 const chalk = require('chalk');
 
-console.log(chalk.cyan('Hello world!'));
+console.log('It is ' + chalk.blue.bold(moment(new Date()).format("dddd, MMMM Do YYYY, h:mm:ss a")));
 
-var data = 'Jul 18 2017 20:14:56 GMT-0600 (PST)';
-console.log(moment(new Date(data)).format("[It is] dddd, MMMM Do YYYY, h:mm:ss a"));
+console.log('It is the ' + chalk.magenta.bold(moment(new Date()).format("DDDo")) + ' day of the year.');
+
+let currentTime = moment(new Date());
+let midnight = moment(new Date()).startOf('day');
+let result = currentTime.diff(midnight, 'seconds');
+
+
+console.log('It is ' + chalk.cyan.bold(result) + ' seconds into the day.');
+
+let isDST = '';
+if (moment(new Date()).isDST()) {
+  isDST = 'is';
+} else {
+  isDST = 'is not';
+}
+
+console.log('It ' + chalk.green.bold(isDST) + ' during Daylight Savings Time.');
+
+let isLeapYear = '';
+if (moment(new Date()).isLeapYear()) {
+  isLeapYear = 'is';
+} else {
+  isLeapYear = 'is not';
+}
+
+console.log('It ' + chalk.red.bold(isLeapYear) + ' not a leap year.');
